7227825bba796bf1a2765114b7f70ab280aef47d
riavvxe <valeriaberrospi05@gmail.com>
2026-04-21 18:07:00 +0200

    boton nievl y ranking

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-const ANCHO_MENU=1200;
-const ALTO_MENU= 620;
+const ANCHO_MENU = window.innerWidth;
+const ALTO_MENU = window.innerHeight;
+const COLUMNA_1 = ANCHO_MENU*0.3;
+const COLUMNA_2 = ANCHO_MENU*0.65;
+const ALTURA_1= ALTO_MENU*0.4;
+const ALTURA_2=ALTO_MENU*0.62;
 let menuState={
     preload: function(){
         
@@ -9,23 +13,25 @@ let menuState={
     create: function(){
        // game.scale.setGameSize(window.innerWidth, window.innerHeight);
         let fondo = game.add.sprite(0, 0, "fondo");
-        fondo.width = ANCHO_MENU;
+        fondo.width = ANCHO_MENU;   
         fondo.height = ALTO_MENU;
-        let botonplay = game.add.button(game.world.centerX-250, game.world.centerY-50, 'play1', this.iniciarJuego, this);
+        // fondo.scale.setTo()
+        let botonplay = game.add.button(COLUMNA_1, ALTURA_1, 'play1', this.iniciarJuego, this);
         botonplay.anchor.setTo(0.5,0.5);
         
-        let botoncred=game.add.button(game.world.centerX-250, game.world.centerY +80, 'play1', this.verCreditos, this);
-        botoncred.anchor.setTo(0.5,0,5);
+        let botoncred=game.add.button(COLUMNA_1, ALTURA_2, 'play1', this.verCreditos, this);
+        botoncred.anchor.setTo(0.5,0.5);
 
-        let botonniv=game.add.button(game.world.centerX+250, game.world.centerY-110 , 'play1', this.verNiveles, this);
-        botonniv.anchor.setTo(0.5,0,5);
+        let botonniv=game.add.button(COLUMNA_2, ALTURA_1 , 'play1', this.verNiveles, this);
+        botonniv.anchor.setTo(0.5,0.5);
 
-        let botonrank=game.add.button(game.world.centerX+250, game.world.centerY +80, 'play1', this.verCreditos, this);
+        let botonrank=game.add.button(COLUMNA_2, ALTURA_2 , 'play1', this.verRanking, this);
+        botonrank.anchor.setTo(0.5,0.5);
 
         
         let estitulo = {font: "100px Arial", fill:"#FF00FF" , align:"center"}
-        let titulo= game.add.text(game.world.centerX - 100, 100,"TETRIS",estitulo);
-        titulo.anchor.setTo(0.2, 0.5);
+        let titulo= game.add.text(game.world.width * 0.5, game.world.height * 0.15,"TETRIS",estitulo);
+        titulo.anchor.setTo(0.5, 0.5);
 
     },
     iniciarJuego : function(){
@@ -35,7 +41,12 @@ let menuState={
     verCreditos: function(){
         game.state.start('Creditos');
     },
-
+    verNiveles: function(){
+        game.state.start('Niveles');
+    },
+    verRanking:function(){
+        game.state.start('Ranking')
+    }
 };
 let creditosState={
     create : function(){
@@ -44,29 +55,45 @@ let creditosState={
         fondo.width = ANCHO_MENU;
         fondo.height = ALTO_MENU;
         let estitulo = {font: "100px Arial", fill:"#1be6f5" , align:"center"}
-        let titulo= game.add.text(game.world.centerX - 100, 100,"CRÉDITOS",estitulo);
-        titulo.anchor.setTo(0.2, 0.5);
-        let botonVolver = game.add.button(game.world.centerX, game.world.centerY+220, 'play1', this.volverMenu, this);
+        let titulo= game.add.text(game.world.width * 0.5, game.world.height * 0.15,"CRÉDITOS",estitulo);
+        titulo.anchor.setTo(0.5, 0.5);
+        let botonVolver = game.add.button(game.world.width * 0.5, game.world.height * 0.8548387096774194, 'play1', this.volverMenu, this);
         botonVolver.anchor.setTo(0.5,0.5);
-        let txtCred = game.add.text(game.world.centerX, game.world.centerY, "Adolfo A. Martinez García\nIker Bélles Barreda\nValeria F. Berrospi Escobar ", { font: "20px Arial", fill: "#000" });
+        let txtCred = game.add.text(game.world.width * 0.5, game.world.height * 0.5, "Adolfo A. Martinez García\nIker Bélles Barreda\nValeria F. Berrospi Escobar ", { font: "20px Arial", fill: "#000" });
         txtCred.anchor.setTo(0.5,0.5);
     },
     volverMenu : function(){
         game.state.start('Menu'); 
-        let fondo = game.add.sprite(0, 0, "fondo");
-        fondo.width = ANCHO_MENU;
-        fondo.height = ALTO_MENU;
-        let estitulo = {font: "100px Arial", fill:"#f5ee1b" , align:"center"}
-        let titulo= game.add.text(game.world.centerX - 100, 100,"NIVELES",estitulo);
-        titulo.anchor.setTo(0.2, 0.5);
-        let botonVolver2 = game.add.button(game.world.centerX, game.world.centerY+220, 'play1', this.volverMenu, this);
-        botonVolver2.anchor.setTo(0.5,0.5);
-
     }
 };
 let nivelesState={
     create : function(){
-
+        let fondo = game.add.sprite(0, 0, "fondo");
+        fondo.width = ANCHO_MENU;
+        fondo.height = ALTO_MENU;
+        let estitulo = {font: "100px Arial", fill:"#f5ee1b" , align:"center"}
+        let titulo= game.add.text(game.world.width * 0.5, game.world.height * 0.15,"NIVELES",estitulo);
+        titulo.anchor.setTo(0.5, 0.5);
+        let botonVolver2 = game.add.button(game.world.width * 0.5, game.world.height * 0.8548387096774194, 'play1', this.volverMenu, this);
+        botonVolver2.anchor.setTo(0.5,0.5);
+    },
+    volverMenu : function(){
+        game.state.start('Menu'); 
+    }
+};
+let rankingState={
+     create : function(){
+        let fondo = game.add.sprite(0, 0, "fondo");
+        fondo.width = ANCHO_MENU;
+        fondo.height = ALTO_MENU;
+        let estitulo = {font: "100px Arial", fill:"#671bf5" , align:"center"}
+        let titulo= game.add.text(game.world.width * 0.5, game.world.height * 0.15,"RANKING",estitulo);
+        titulo.anchor.setTo(0.5, 0.5);
+        let botonVolver2 = game.add.button(game.world.width * 0.5, game.world.height * 0.8548387096774194, 'play1', this.volverMenu, this);
+        botonVolver2.anchor.setTo(0.5,0.5);
+    },
+    volverMenu : function(){
+        game.state.start('Menu'); 
     }
 }
 
@@ -74,6 +101,8 @@ let game = new Phaser.Game(ANCHO_MENU, ALTO_MENU, Phaser.CANVAS, 'game');
 game.state.add("Menu",menuState);
 game.state.add('Game', gameState);
 game.state.add("Creditos",creditosState);
+game.state.add("Niveles",nivelesState);
+game.state.add("Ranking",rankingState);
 
 game.state.start("Menu");
 
